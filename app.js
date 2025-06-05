a9ffa6e1b840cffabf079fe3379706a0bb656e6e
Flory <flo.kadima@gmail.com>
2025-06-05 16:20:43 +0200

    configuration de l'url de la base des données

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+});
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Bienvenue dans l'API !");
+});
+
+app.get("/ping", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT 'Connexion OK' AS message");
+        res.send(result.rows[0].message);
+    } catch (err) {
+        console.error("Erreur PostgreSQL :", err);
+        res.status(500).send("Erreur de connexion");
+    }
+});
+
+module.exports = app;
